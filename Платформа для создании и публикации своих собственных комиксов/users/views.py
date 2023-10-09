b12ba105c19044aa2e6328988f0fd884ddc88d2e
zv_nik <105583773+ZverevNikita@users.noreply.github.com>
2023-10-09 20:19:18 +0300

    Определения представлений для пользователей

--- Платформа для создании и публикации своих собственных комиксов/users/views.py
+++ Платформа для создании и публикации своих собственных комиксов/users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
 from users.models import User
 
 
-class UserCreationForm
+class UserCreationForm(BaseUserCreationForm):
     class Meta(BaseUserCreationForm.Meta):
         model = User
 
