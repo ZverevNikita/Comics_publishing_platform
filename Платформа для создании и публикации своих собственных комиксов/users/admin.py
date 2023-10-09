4a7065f2b7555993930b9c0eb925bbf7f8bf7ca9
zv_nik <105583773+ZverevNikita@users.noreply.github.com>
2023-10-09 19:02:36 +0300

    Delete Платформа для создании и публикации своих собственных комиксов directory

--- Платформа для создании и публикации своих собственных комиксов/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.admin import AdminSite
-from django.contrib.auth.models import Group
-
-admin.site.unregister(Group)
-
-AdminSite.site_title = 'Комиксы'
-AdminSite.site_header = 'Комиксы'
