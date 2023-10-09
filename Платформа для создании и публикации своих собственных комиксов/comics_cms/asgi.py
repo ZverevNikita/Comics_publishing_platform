dc3f663f3c968021edcdc5d65f5bfa8edcf30a24
zv_nik <105583773+ZverevNikita@users.noreply.github.com>
2023-10-09 20:58:21 +0300

    htt

--- Платформа для создании и публикации своих собственных комиксов/comics_cms/asgi.py
+++ Платформа для создании и публикации своих собственных комиксов/comics_cms/asgi.py
@@ -3,7 +3,7 @@ ASGI config for comics_cms project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
