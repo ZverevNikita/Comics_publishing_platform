ee9dcab54689479a640851b8682027ab7d46ff78
zv_nik <105583773+ZverevNikita@users.noreply.github.com>
2023-10-09 20:10:29 +0300

    gdrg

--- Платформа для создании и публикации своих собственных комиксов/comics/apps.py
+++ Платформа для создании и публикации своих собственных комиксов/comics/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ComicsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'comics'
-    verbose_name = 'Комиксы'
+    verbose_name = 
