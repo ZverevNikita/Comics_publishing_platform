2a9fdd561af4d92e0881763dde10822c0f7f03b9
zv_nik <105583773+ZverevNikita@users.noreply.github.com>
2023-10-09 20:11:00 +0300

    Конфигурация приложения комиксов

--- Платформа для создании и публикации своих собственных комиксов/comics/apps.py
+++ Платформа для создании и публикации своих собственных комиксов/comics/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ComicsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'comics'
-    verbose_name = 
+    verbose_name = 'Комиксы'
